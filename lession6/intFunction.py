839412bf076d07d7891cf54c754d6062de4122e9
jasujon <jubayedalamsujon@gmail.com>
2020-01-13 01:25:57 +0600

    Int() function : python

---
+++ lession6/intFunction.py
@@ -0,0 +1,23 @@
+#----------------------------Int() function : python -------------------------------
+
+# number_one = input("Enter First Number ")  # 4
+# number_two = input("Enter Secend Number ") # 4
+# total = number_one + number_two
+# print("Total Value : " + total)
+#     # output : 44
+
+
+number_one = int(input("Enter First Number "))  # 4
+number_two = int(input("Enter Secend Number ")) # 4
+total = number_one + number_two
+print("Total Value : " + str(total))
+
+    #out put 8
+
+#float
+number1 = str(4)
+number2 = int(33)
+number3 = float(44)
+
+print(number2+number3)
+#output = 77.00
