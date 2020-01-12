35cc91c4e8530b913146d359aae99c6b1413b954
jasujon <jubayedalamsujon@gmail.com>
2020-01-12 23:50:07 +0600

    variable

---
+++ 1-lession3/variable.py
@@ -0,0 +1,34 @@
+# number1=2
+# print(number1)
+# number1=4
+# print(number1)
+# #string,number
+# name = 'Jubayed Alam'
+# print(name)
+# name = 123
+# print(name)
+
+# #rule one
+# 1number = 123
+# print(1number)
+
+# #rule two
+# $name = 123
+# print($name)
+
+# #rule three
+# %name = 123
+# print(%name)
+
+##right rules
+# name = 123
+# print(name)
+
+# name1=123
+# print(name1)
+
+# _name = 123
+# print(_name)
+
+#user_one_name = Snake case Writing
+#userOneName = Camel Case Writting
