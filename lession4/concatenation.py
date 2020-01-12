f9e25f37bae7ac7d7ea200c7d15a400e9ec182f3
jasujon <jubayedalamsujon@gmail.com>
2020-01-13 00:14:23 +0600

    String Concatenation

---
+++ lession4/concatenation.py
@@ -0,0 +1,15 @@
+# ------------------String Concatenation ---------------------
+
+# first_name ='Jubayed'
+# last_name = 'Alam'
+# full_name = first_name + last_name
+# print(full_name)
+
+first_name ='Jubayed'
+last_name = 'Alam'
+full_name = first_name + " "+last_name
+#print(full_name +3) #output show type Error
+# print(full_name +"3" ) #no Error
+
+# print(full_name + str(3)) #no Error
+
