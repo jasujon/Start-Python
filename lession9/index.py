722f3065a2bf1ddc408d42381a4a609c5a5cb8c7
jasujon <jubayedalamsujon@gmail.com>
2020-01-13 22:14:37 +0600

    String formatting

---
+++ lession9/index.py
@@ -0,0 +1,21 @@
+#-------------------------String formatting----------------------------------
+
+#normal 
+name = "Jubayed Alam "
+age  = 24
+print("Hello " + name + "Your Age is " + str(age))
+
+
+
+##python 3
+
+name = "Sultan Alam "
+age  = 10
+print("Hello {} Your Age Is {}".format(name,age))
+
+
+##python 3.6
+
+name = "Mohiuddin "
+age  = 29
+print(f"Hello {name} Your Age is {age}")
