4b7742d2a00d5c3ba8580f914b781db33ecdac10
jasujon <jubayedalamsujon@gmail.com>
2020-01-14 23:31:29 +0600

    if else StateMent

--- lession13/index.py
+++ lession13/index.py
@@ -8,7 +8,15 @@
 
 #-----------------------Pass StateMent--------------------  
 
-x = 18
-if x>18:
-    pass
-#output null
+# x = 18
+# if x>18:
+#     pass
+# #output null
+
+#-----------------------if else StateMent-------------------- 
+
+age = int(input("Enter Your Age : "))
+if age >= 10:
+    print("You Can Play The Game")
+else:
+    print("You Can't Play The Game")
