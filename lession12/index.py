2b3be3c1579fcd00614f51bf6386616ec2b4933d
jasujon <jubayedalamsujon@gmail.com>
2020-01-14 21:36:47 +0600

    Strip Method $ Replace Method

--- lession12/index.py
+++ lession12/index.py
@@ -27,3 +27,29 @@ print(name3.title())
 name4 = "Jubayed Alam"
 print(name4.count("a"))
 #output   2
+
+
+#--------------------------Strip Method $ Replace Method----------------------- 
+
+lang = "        Python            "
+dots ="............"
+print(lang+dots)
+#output :          Python            ............
+
+#Remove Left Space
+print(lang.lstrip() + dots)
+#output: Python            ............
+
+#Remove Right Space 
+print(name.rstrip() + dots)
+#output:   Python............
+
+#Remove All Space
+print(name.strip() + dots)
+#output:Python............
+
+
+####Replace Method
+lang1 = "Pyt     hon"
+print(lang1.replace(" " , ""))
+#output: Python
