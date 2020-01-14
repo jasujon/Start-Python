182c93b3a16b038ca783d743b2e6d979400bc5dd
jasujon <jubayedalamsujon@gmail.com>
2020-01-14 23:27:27 +0600

    Pass StateMent

--- lession13/index.py
+++ lession13/index.py
@@ -1,7 +1,14 @@
 #-----------------------If StateMent-------------------- 
 
-age = input("Enter Your Age : ")
-age = int(age)
-if age >= 14 :
-    #print("Line a ")
-    print("You Are Above 14")
+# age = input("Enter Your Age : ")
+# age = int(age)
+# if age >= 14 :
+#     #print("Line a ")
+#     print("You Are Above 14")
+
+#-----------------------Pass StateMent--------------------  
+
+x = 18
+if x>18:
+    pass
+#output null
