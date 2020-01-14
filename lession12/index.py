da0544b0b28d485d27e044585827a2c35fa2a8ef
jasujon <jubayedalamsujon@gmail.com>
2020-01-14 20:49:50 +0600

    String Methods(Function & Method)

---
+++ lession12/index.py
@@ -0,0 +1,29 @@
+#-----------------------String Methods(Function & Method) ----------------- 
+
+
+name = "Python"
+#len() function
+print(len(name))
+#output   6
+
+
+#lower() Method
+name1 = "JUBAYED ALAM"
+print(name1.lower()) 
+#output  jubayed alam
+
+
+#upper() Method
+name2 = "jubayed alam"
+print(name2.upper())
+#output  JUBAYED ALAM
+
+#title() method
+name3 = "JUBAYED ALAM"
+print(name3.title())
+#output Jubayed Alam
+
+#count() Method
+name4 = "Jubayed Alam"
+print(name4.count("a"))
+#output   2
