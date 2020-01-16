920e26399526b8f8496c52c0580a7660a1664fda
jasujon <jubayedalamsujon@gmail.com>
2020-01-16 23:44:46 +0600

    Functions Intro & Function Exm

---
+++ lession14/index.py
@@ -0,0 +1,30 @@
+#-----------------------------Functions Intro & Function Exm --------------------------------- 
+
+#add two number
+# def add_two(a,b):
+#     return a+b
+# total = add_two(5,5)
+# print(total)
+
+#output 10
+
+#get input user and print total
+
+# def add_two(num1,num2):
+#     return num1+num2
+
+# a = int(input("Enter First Number : ")) #10
+# b = int(input("Enter Second Number : ")) #10
+
+# total = a+b
+# print(total)
+#output : 20
+
+
+#get user first Name
+
+def user_name(a,b):
+    return a + b
+firstName = input("Enter Your First Name : ")
+lastName  = input("Enter Your Last Name : ")
+print(user_name(firstName,lastName))
