6eecba88d7e9da82cf3f3ecf6493da9b727b48b7
jasujon <jubayedalamsujon@gmail.com>
2020-01-18 20:07:50 +0600

    Python List Intro

---
+++ lession15/index.py
@@ -0,0 +1,21 @@
+#------------------------- Python List Intro------------------------------ 
+#rules for  making list 
+#int
+numbers = [1,2,3,4,5]
+print(numbers) 
+#output :  [ 1,2,3,4]
+
+#string
+names = ['Python','Php','javaScript']
+print(names)
+#output : ['Python', 'Php', 'javaScript'] 
+
+#mix all datatype
+mixed = [1,2,'Python','javaScript',2.3,None]
+print(mixed)
+#output : [1, 2, 'Python', 'javaScript', 2.3, None]
+
+#access Element
+mixed = [1,2,'Python','javaScript']
+print(mixed[1])
+#output: 2
