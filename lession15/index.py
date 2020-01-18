ec5b7891a21b61b17a548df88241c7a97ec3f007
jasujon <jubayedalamsujon@gmail.com>
2020-01-18 22:41:19 +0600

    Clear Method

--- lession15/index.py
+++ lession15/index.py
@@ -123,6 +123,13 @@
 #output : [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 #-------------------------sorted Function ------------------------------ 
-numbers = [9,8,7,6,5,4,3,2,1]
-print(sorted(numbers))
+# numbers = [9,8,7,6,5,4,3,2,1]
+# print(sorted(numbers))
 #output : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+#-------------------------Clear Method ------------------------------ 
+numbers = [9,8,7,6,5,4,3,2,1]
+numbers.clear()
+print(numbers)
+#output : []
