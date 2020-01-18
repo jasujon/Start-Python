e3d7d4fc0d3e932fe361b9a96db7d207574a0a3c
jasujon <jubayedalamsujon@gmail.com>
2020-01-18 20:29:26 +0600

    More methods to add data (insert() Method)

--- lession15/index.py
+++ lession15/index.py
@@ -23,17 +23,24 @@
 
 #-------------------------Add data to list (Append Method)------------------------------ 
 #Append() Method 
-fruits = ['grapes','apple']
-fruits.append('Mango')
-print(fruits)
-#output: ['grapes', 'apple', 'Mango']
+# fruits = ['grapes','apple']
+# fruits.append('Mango')
+# print(fruits)
+# #output: ['grapes', 'apple', 'Mango']
 
-#Real Life use Append Method
-fruits = []
-fruits.append('Mango')
-fruits.append('Grapes')
-fruits.append('Apple')
-fruits.append('Banana')
-print(fruits)
-#output : ['Mango', 'Grapes', 'Apple', 'Banana']
+# #Real Life use Append Method
+# fruits = []
+# fruits.append('Mango')
+# fruits.append('Grapes')
+# fruits.append('Apple')
+# fruits.append('Banana')
+# print(fruits)
+# #output : ['Mango', 'Grapes', 'Apple', 'Banana']
 
+#-------------------------More methods to add data (insert() Method)------------------------------
+
+#insert() Method
+fruits1 =['Mango','Banana']
+fruits1.insert(0,'Grapes')
+print(fruits1) 
+#output: ['Grapes', 'Mango', 'Banana']
