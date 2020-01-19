7b624c42659022e8ffae6da5f87762b0eaf489d6
jasujon <jubayedalamsujon@gmail.com>
2020-01-19 23:48:52 +0600

    Tuples Intro

---
+++ lession16/index.py
@@ -0,0 +1,16 @@
+#------------------------------Tuples Intro--------------------------- 
+
+#Tuples Data Structure
+#Tuples Store Any Datatype (like List)
+#Most important tuples are immutable, once tuples is created you cant update 
+
+example = ('one','two','three')     #this is a tuples
+#now you cant no append , no insert , no pop , no remove
+#tuples are faster than list
+
+
+#Tuples can  use Method ()
+#count 
+#index
+#len function
+#slicing 
