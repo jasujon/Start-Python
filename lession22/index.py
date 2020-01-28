e4e1bd21db6d60780dcd54657586f29df7112f68
jasujon <jubayedalamsujon@gmail.com>
2020-01-28 22:26:25 +0600

    Lambda Expression Practice

--- lession22/index.py
+++ lession22/index.py
@@ -44,3 +44,35 @@
 
 
 
+
+
+#------------------------------------Lambda Expression Practice-----------------------------
+
+#normal function
+# def is_even(a):
+#     if a % 2 == 0 :
+#         return True
+#     else:
+#         return False
+# print(is_even(5))
+# #output : 5
+
+
+#shorted function in is_even function :
+
+# def is_even(a):
+#     return a % 2 == 0 
+# print(is_even(4))
+#output: True
+
+
+
+#Lambda Expression use this function
+
+is_even = lambda a : a % 2 == 0
+print(is_even(10))
+#output : True
+
+
+
+
