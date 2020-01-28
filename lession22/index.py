ee95adc76dd4655510fe7ff9ae79f42484ce0e37
jasujon <jubayedalamsujon@gmail.com>
2020-01-28 22:17:50 +0600

    Lambda Expression (anonymous function)

---
+++ lession22/index.py
@@ -0,0 +1,46 @@
+#------------------------------------Lambda Expression (anonymous function)-----------------------------
+
+
+#lambda expression / anonymous function means this function don't  have name
+
+
+#normal function
+# def add (a,b):
+#     return a+b
+
+
+#lambda Expression / anonymous function
+# add2 = lambda a,b : a+b
+# print(add2(5,3))
+# #output : 8
+# print(type(add2))
+# #output : <class 'function'>
+
+
+# multiply = lambda a,b : a*b 
+# print(multiply(5,5))
+# #output : 25
+
+
+# print(add2)
+#<function <lambda> at 0x00E80388>              # lambda and location show
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
