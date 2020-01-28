0ca3fe02655d3d808b5b66ca7f5e644e0345118e
jasujon <jubayedalamsujon@gmail.com>
2020-01-29 00:01:29 +0600

    Filter Function

--- lession23/index.py
+++ lession23/index.py
@@ -10,7 +10,7 @@
  #How we can do this without enumerate function
  #
 
-names = ['Jubayed','Alam','Sujon']
+# names = ['Jubayed','Alam','Sujon']
  # suppose we need our result like this :  0 ---> Jubayed , 1---> Alam
 
 # position = 0
@@ -27,9 +27,104 @@ names = ['Jubayed','Alam','Sujon']
 #
 #with enumerate function
 #
-for position,name in enumerate(names):
-    print(f"{position} ---- > {name}")
+# for position,name in enumerate(names):
+#     print(f"{position} ---- > {name}")
 #output : 
 # 0 ---- > Jubayed
 # 1 ---- > Alam
 # 2 ---- > Sujon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------Map function-----------------------------  
+
+# numbers = [1,2,3,4,5]
+
+# #
+# # without lambda
+# #
+# def square(a):
+#     return a**2
+# square1 = list(map(square,numbers))         # number convert list 
+# print(square1)
+# #output : [1, 4, 9, 16, 25]
+
+
+# #
+# # with lambda
+# #
+# square = list(map(lambda a: a**2 , numbers))
+# print(square)
+# #output : [1, 4, 9, 16, 25]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------Filter Function----------------------------- 
+
+#
+# show just even number
+#
+
+numbers = [1,2,4,6,8,35,76,6]
+
+def is_even(a):
+    return a % 2 == 0
+
+even = tuple(filter(is_even,numbers))
+print(even)
+
+#output : (2, 4, 6, 8, 76, 6)
