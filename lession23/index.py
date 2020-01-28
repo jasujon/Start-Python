67409dcf1edb76517f407ec8ed4e0002e55d0b9b
jasujon <jubayedalamsujon@gmail.com>
2020-01-29 00:23:12 +0600

    Zip Function

--- lession23/index.py
+++ lession23/index.py
@@ -119,12 +119,81 @@
 # show just even number
 #
 
-numbers = [1,2,4,6,8,35,76,6]
+# numbers = [1,2,4,6,8,35,76,6]
 
-def is_even(a):
-    return a % 2 == 0
+# def is_even(a):
+#     return a % 2 == 0
 
-even = tuple(filter(is_even,numbers))
-print(even)
+# even = tuple(filter(is_even,numbers))
+# print(even)
 
 #output : (2, 4, 6, 8, 76, 6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------Zip Function----------------------------- 
+user_id   = ['user1','user2','user3']
+user_name = ['Jubayed','Alam','Sujon']
+
+print(list(zip(user_id,user_name)))
+#output : [('user1', 'Jubayed'), ('user2', 'Alam'), ('user3', 'Sujon')]
+
+
+example = [('a',1),('b',2)]
+print(dict(example))
+#output : {'a': 1, 'b': 2}
+
+
+
+
+
+l = [(1,2),(3,4),(5,6),(6,7)]
+#
+#show : l1 = [1, 3, 5, 6]   l2=[2, 4, 6, 7] from l
+#
+
+
+#we use  * operator with zip
+
+l1,l2 =list(zip(*l))
+print(l1)
+print(l2)
+#output : 
+    #(1, 3, 5, 6)
+    #(2, 4, 6, 7)
+
