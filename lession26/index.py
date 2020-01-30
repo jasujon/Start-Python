01fbf0c28e18f70c9347f04b2e3d0c81aef0ca5d
jasujon <jubayedalamsujon@gmail.com>
2020-01-31 02:54:10 +0600

    oop intro(Object Oriented Programing)

---
+++ lession26/index.py
@@ -0,0 +1,21 @@
+#------------------------- oop intro(Object Oriented Programing)-------------------------
+
+
+#object oriented programing
+#common topic in almost all popular programing language (python , javaScript, Php , c,C++) with common      idea but different syntex
+# oop just style or way to write a code
+# oop very helpful to creating real world programs
+# 
+# 
+# 
+# we hearing many time in oop like 1. Class 2. Object(instance) 3. Method
+# 
+# 
+# list class
+# list object
+# list method
+# 
+# i = [1,2,3,4,5]
+# i.append(d)           # hear append is a method
+#
+#And i in class Object
