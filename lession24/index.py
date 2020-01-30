5f1ea9f6878b925f84069ed2a9767296701c459a
jasujon <jubayedalamsujon@gmail.com>
2020-01-30 23:23:02 +0600

    Pass function as argument

--- lession24/index.py
+++ lession24/index.py
@@ -7,21 +7,74 @@
 #
 
 
+# def square(a):
+#     return a**2
+# # s=square(7)
+# # print(s)
+
+# #output : 49
+
+# s = square
+# # print(s(7))
+# #output : 49            # same result 
+
+# #find function name 
+# print(s.__name__)
+# #output : square
+# print(s)                #for find location
+# #output : <function square at 0x037F0340>
+# print(square)           #for find location
+# #output : <function square at 0x037F0340>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------Pass function as argument------------------------------
+
+
+# #map function 
+# def square(a):
+#     return a**2
+# l=[1,2,3,4,5]
+# print(list(map(square,l)))              #take function as a argument
+# #output : [1, 4, 9, 16, 25]             #square all the value
+
+
 def square(a):
     return a**2
-# s=square(7)
-# print(s)
 
-#output : 49
+l = [1,2,3,4,5]
 
-s = square
-# print(s(7))
-#output : 49            # same result 
+def my_map(func , l ):
+    new_list = []
+    for item in l:
+        new_list.append(func(item))
+    return new_list
 
-#find function name 
-print(s.__name__)
-#output : square
-print(s)                #for find location
-#output : <function square at 0x037F0340>
-print(square)           #for find location
-#output : <function square at 0x037F0340>
+print(my_map(square,l))
+#output : [1, 4, 9, 16, 25]
+
